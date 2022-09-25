87a7af845544530cdfb3d8335455cb614b63d811
lonelygamer01 <lonelygamer04@gmail.com>
2022-09-25 17:56:06 +0200

    update 2022-09-25

--- Car_register.py
+++ Car_register.py
@@ -8,8 +8,8 @@ animation = "|-"
 print("C A R - R E G I S T E R")
 time.sleep(2)
 print("L O A D I N G")
-for i in range(50):
-    time.sleep(0.2)
+for i in range(90):
+    time.sleep(0.1)
     sys.stdout.write("\r" + animation[i % len(animation)])
     sys.stdout.flush()
 time.sleep(2)
@@ -65,7 +65,7 @@ if number == 2:
         print("1. Register a new car\n2. See the details of a car\n3. Edit data of a car\n4. Edit username\n5. Edit password\n6. Delete Account")
 
         number = int(input("Enter the number: \n"))
-        #Registerin a new car IN DEVELOPMENT!!
+        #Registerin a new car FINISHED
         if number == 1:
             brand = input("Brand: ")
             model = input("Model: ")
@@ -80,107 +80,19 @@ if number == 2:
             plate_number = input("Platenumber: ")
             km = input("Km: ")
             condition = input("Condition: ")
+            time.sleep(2)
+            print("\nProcessing...")
 
-            class Car:
-                def __init__(self, brand, model, Class, year, type, color, weight, gearbox, engine, hp, plate_number, km, condition):
-                    self.brand = brand
-                    self.model = model
-                    self.Class = Class
-                    self.year = year
-                    self.type = type
-                    self.color = color
-                    self.weight = weight
-                    self.gearbox = gearbox
-                    self.engine = engine
-                    self.hp = hp
-                    self.plate_number = plate_number
-                    self.km = km
-                    self.condition = condition
+            file_name = f"{plate_number}_data.json"
 
-                def get_info(self):
-                    print("Model: {}, Class: {}, Year: {}, Type: {}, Color: {}, Weight: {}, Gearbox: {}, Engine: {}, Hp: {}, Plate number: {}, Km: {}, Condition: {}".format(self.model, self.Class, self.year, self.type, self.color, self.weight, self.gearbox,self.engine,self.hp, self.plate_number, self.km, self.condition))
-
-                def update_model(self, file, model):
-                    a_file = open(file, "r")
-                    json_object = json.load(a_file)
-                    a_file.close()
-                    self.model = model
-                    json_object["Model"] = self.model
-                    a_file = open(file, "w")
-                    json.dump(json_object, a_file, indent=2)
-                    a_file.close()
-
-                def update_color(self, file, color):
-                    a_file = open(file, "r")
-                    json_object = json.load(a_file)
-                    a_file.close()
-                    self.color = color
-                    json_object["Color"] = self.color
-                    a_file = open(file, "w")
-                    json.dump(json_object, a_file, indent=2)
-                    a_file.close()
-
-                def update_km(self, file, km):
-                    a_file = open(file, "r")
-                    json_object = json.load(a_file)
-                    a_file.close()
-                    self.km = km
-                    json_object["Km"] = self.km
-                    a_file = open(file, "w")
-                    json.dump(json_object, a_file, indent=2)
-                    a_file.close()        
-
-                def save_to_json(self,filename):
-                    car_data = {"Brand":self.brand, "Model":self.model, "Class":self.Class, "Year":self.year, "Type": self.type, "Color":self.color, "Weight":self.weight, "Gearbox":self.gearbox, "Engine":self.engine, "Hp":self.hp, "Plate_number": self.plate_number, "Km":self.km, "Condition":self.condition}    
-                    with open(filename, "w") as file:
-                        file.write(json.dumps(car_data, indent=3))
-
-                def load_from_json(self, filename):
-                    with open(filename, "r") as file:
-                        data = json.loads(file.read())
-
-                    self.name = data["name"]
-                    self.point = data["point"]
-                    self.current_streak = data["streak"]
-
-                
-
-            c1 = Car(brand, model, Class, year, type, color, weight, gear_box, engine, hp, plate_number, km, condition)
-            c1.save_to_json("{}_data.json".format(c1.plate_number))
-            c1.get_info()
-
-            update_model = input("Do you want to update the model's name? (y/n): ")
-            if update_model == "y":
-                file_name = input("Enter the filename you want to update: ")
-                model_name = input("Enter the new model name: ")
-                c1.update_model(file_name, model_name)
-                c1.save_to_json("{}_data.json".format(c1.plate_number))
-                print('Update was successful...')
-                print('New model name is: {}'.format(c1.model))
-            else:
-                pass
-
-            update_color = input("Do you want to update the model's color? (y/n): ")
-            if update_color == "y":
-                file_name = input("Enter the filename you want to update: ")
-                model_color = input("Enter the new color: ")
-                c1.update_color(file_name, model_color)
-                c1.save_to_json("{}_data.json".format(c1.plate_number))
-                print('Update was successful...')
-                print('New color is: {}'.format(c1.color))
-            else:
-                pass
-
-            update_km = input("Do you want to update the model's km? (y/n): ")
-            if update_km == "y":
-                file_name = input("Enter the filename you want to update: ")
-                model_km = input("Enter the new km: ")
-                c1.update_km(file_name, model_km)
-                c1.save_to_json("{}_data.json".format(c1.plate_number))
-                print('Update was successful...')
-                print('New km is: {}'.format(c1.km))
-            else:
-                pass
+            def save_to_json(filename):
+                car_data = {"Brand":brand, "Model":model, "Class":Class, "Year":year, "Type": type, "Color":color, "Weight":weight, "Gearbox":gear_box, "Engine":engine, "Hp":hp, "Plate_number": plate_number, "Km":km, "Condition":condition}
+                with open(filename, "w") as file_save:
+                    file_save.write(json.dumps(car_data, indent=3))
+                file_save.close()
+            save_to_json(file_name)
+            time.sleep(2)
+            print("New car has been added to the database")
         #Displaying the data of a car from the database FINISHED
         if number == 2:
             plate_number = input("Enter the platenumber of the car please:\n")
@@ -206,9 +118,13 @@ if number == 2:
             plate_number_to_json = "{}_data.json".format(plate_number)
 
             get_info(plate_number_to_json)
-        #Editing a cars details in the database...
+        #Editing a cars details in the database DEVELOPMENT
         if number == 3:
-            pass
+            print("Enter the number from the list below, what you would like to change in the database of you car!")
+            print("1. Edit Brand\n2. Edit Modle\n3. Edit Class\n3. Edit Year\n4. Edit Type\n5. Edit Color\n6. Edit Weight\n7. Edit Gear Box\n8. Edit Engine\n9. Edit Horsepower\n10. Edit Kilometer\n11. Edit Condition")
+            open_file = input("Enter the platenumber of the car, please make sure you enter it correctly!\n")
+            #file = f"{open_file}_data.json"
+
         #Editing username FINISHED
         if number == 4:
             old_username = input("Enter the current username:\n")
@@ -253,7 +169,7 @@ if number == 2:
                     print('The password isnt correct...')
                     
                 file_edit1.close()
-
+        #Deleting the account info FINISHED
         if number == 6:
             username_delete = input("Enter the username:\n")
             password_delete = input("Enter the password:\n")
@@ -267,7 +183,7 @@ if number == 2:
                     print("Log-In was successful")
                     file_delete1.close()
                     time.sleep(1)
-                    ensure = input("Press y to continue deleting details...")
+                    ensure = input("Press y and hit Enter to continue deleting details...\n")
                     if ensure == "y":
                         data_delete = {"Username": "", "Password": ""}
                         with open("User_data.json", "w") as file_delete2:
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import json
 
 
 
-plate_number = input("Enter the platenumber of the car please:\n")
+'''plate_number = input("Enter the platenumber of the car please:\n")
 def get_info(filename):
     with open(filename, "r") as file_info:
         data = json.loads(file_info.read())
@@ -18,4 +18,140 @@ def get_info(filename):
     )
 plate_number_to_json = "{}_data.json".format(plate_number)
 
-get_info(plate_number_to_json)    
+get_info(plate_number_to_json)'''
+
+brand = input("Brand: ")
+model = input("Model: ")
+Class = input("Class: ")
+year = input("Year: ")
+type = input("Type: ")
+color = input("Color: ")
+weight = input("Weight: ")
+gear_box = input("Gearbox type: ")
+engine = input("Engine type: ")
+hp = input("Horsepower: ")
+plate_number = input("Platenumber: ")
+km = input("Km: ")
+condition = input("Condition: ")
+
+file_name = f"{plate_number}_data.json"
+
+def save_to_json(filename):
+    car_data = {"Brand":brand, "Model":model, "Class":Class, "Year":year, "Type": type, "Color":color, "Weight":weight, "Gearbox":gear_box, "Engine":engine, "Hp":hp, "Plate_number": plate_number, "Km":km, "Condition":condition}
+    with open(filename, "w") as file_save:
+        file_save.write(json.dumps(car_data, indent=3))
+    file_save.close()
+save_to_json(file_name)
+
+'''brand = input("Brand: ")
+            model = input("Model: ")
+            Class = input("Class: ")
+            year = input("Year: ")
+            type = input("Type: ")
+            color = input("Color: ")
+            weight = input("Weight: ")
+            gear_box = input("Gearbox type: ")
+            engine = input("Engine type: ")
+            hp = input("Horsepower: ")
+            plate_number = input("Platenumber: ")
+            km = input("Km: ")
+            condition = input("Condition: ")
+
+            class Car:
+                def __init__(self, brand, model, Class, year, type, color, weight, gearbox, engine, hp, plate_number, km, condition):
+                    self.brand = brand
+                    self.model = model
+                    self.Class = Class
+                    self.year = year
+                    self.type = type
+                    self.color = color
+                    self.weight = weight
+                    self.gearbox = gearbox
+                    self.engine = engine
+                    self.hp = hp
+                    self.plate_number = plate_number
+                    self.km = km
+                    self.condition = condition
+
+                def get_info(self):
+                    print("Model: {}, Class: {}, Year: {}, Type: {}, Color: {}, Weight: {}, Gearbox: {}, Engine: {}, Hp: {}, Plate number: {}, Km: {}, Condition: {}".format(self.model, self.Class, self.year, self.type, self.color, self.weight, self.gearbox,self.engine,self.hp, self.plate_number, self.km, self.condition))
+
+                def update_model(self, file, model):
+                    a_file = open(file, "r")
+                    json_object = json.load(a_file)
+                    a_file.close()
+                    self.model = model
+                    json_object["Model"] = self.model
+                    a_file = open(file, "w")
+                    json.dump(json_object, a_file, indent=2)
+                    a_file.close()
+
+                def update_color(self, file, color):
+                    a_file = open(file, "r")
+                    json_object = json.load(a_file)
+                    a_file.close()
+                    self.color = color
+                    json_object["Color"] = self.color
+                    a_file = open(file, "w")
+                    json.dump(json_object, a_file, indent=2)
+                    a_file.close()
+
+                def update_km(self, file, km):
+                    a_file = open(file, "r")
+                    json_object = json.load(a_file)
+                    a_file.close()
+                    self.km = km
+                    json_object["Km"] = self.km
+                    a_file = open(file, "w")
+                    json.dump(json_object, a_file, indent=2)
+                    a_file.close()        
+
+                def save_to_json(self,filename):
+                    car_data = {"Brand":self.brand, "Model":self.model, "Class":self.Class, "Year":self.year, "Type": self.type, "Color":self.color, "Weight":self.weight, "Gearbox":self.gearbox, "Engine":self.engine, "Hp":self.hp, "Plate_number": self.plate_number, "Km":self.km, "Condition":self.condition}    
+                    with open(filename, "w") as file:
+                        file.write(json.dumps(car_data, indent=3))
+
+                def load_from_json(self, filename):
+                    with open(filename, "r") as file:
+                        data = json.loads(file.read())
+
+                    self.name = data["name"]
+                    self.point = data["point"]
+                    self.current_streak = data["streak"]
+
+                
+
+            
+
+            update_model = input("Do you want to update the model's name? (y/n): ")
+            if update_model == "y":
+                file_name = input("Enter the filename you want to update: ")
+                model_name = input("Enter the new model name: ")
+                c1.update_model(file_name, model_name)
+                c1.save_to_json("{}_data.json".format(c1.plate_number))
+                print('Update was successful...')
+                print('New model name is: {}'.format(c1.model))
+            else:
+                pass
+
+            update_color = input("Do you want to update the model's color? (y/n): ")
+            if update_color == "y":
+                file_name = input("Enter the filename you want to update: ")
+                model_color = input("Enter the new color: ")
+                c1.update_color(file_name, model_color)
+                c1.save_to_json("{}_data.json".format(c1.plate_number))
+                print('Update was successful...')
+                print('New color is: {}'.format(c1.color))
+            else:
+                pass
+
+            update_km = input("Do you want to update the model's km? (y/n): ")
+            if update_km == "y":
+                file_name = input("Enter the filename you want to update: ")
+                model_km = input("Enter the new km: ")
+                c1.update_km(file_name, model_km)
+                c1.save_to_json("{}_data.json".format(c1.plate_number))
+                print('Update was successful...')
+                print('New km is: {}'.format(c1.km))
+            else:
+                pass'''
